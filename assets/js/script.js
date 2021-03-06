b992345cf493c693c9e1284de4a965a391f28ce3
Matthew Goad <matthewxgoad@gmail.com>
2021-03-06 13:01:51 -0800

    nested row elements in wrappers, fixed time aware class changes, updated layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,32 @@ var clearBtn = $('.clearBtn');
 // Build initial HTML rows
 function buildRows() {
     for( i = 0; i < 10; i++){
+        var eachHour = i + 8;
+        // Create a wrapper to hold each row of the planner
+        var wrapper = document.createElement('div');
+        wrapper.setAttribute('id', 'h' + i + 'time');
+        // Give it the time-block and row classes for styling and layout
+        wrapper.setAttribute('class', 'time-block row')
         // create the first column and populate with the hour
         var timeHour = document.createElement('div');
-        timeHour.setAttribute('class', 'row timeblock hour');
-        timeHour.setAttribute('id', 'h' + i + 'time');
-        var eachHour = i + 8;
+        // Add hour and bootstrap grid classses
+        timeHour.setAttribute('class', 'hour col-md-1');
         timeHour.textContent = (eachHour + ":00");
-        hourlyEl.appendChild(timeHour);
+        wrapper.appendChild(timeHour);
         // create the middle column and populate with a text area
         var eventDesc = document.createElement('textarea');
-        eventDesc.setAttribute('class', 'row future');
+        // Add styling and bootstrap grid classses
+        eventDesc.setAttribute('class', 'description col-md-10 future');
         eventDesc.setAttribute('id', 'textarea' + i );
-        hourlyEl.appendChild(eventDesc);
+        wrapper.appendChild(eventDesc);
         // create the right column and populate with save button icon
         var saveBtn = document.createElement('button');
-        saveBtn.setAttribute('class', 'row saveBtn');
+        // Add styling and bootstrap grid classes
+        saveBtn.setAttribute('class', 'col-md-1 saveBtn');
         saveBtn.setAttribute('id', i);
-        hourlyEl.appendChild(saveBtn);
+        wrapper.appendChild(saveBtn);
+        hourlyEl.appendChild(wrapper);
+        // Add schedule icon to button
         saveBtn.innerHTML = "<img src='assets/images/schedule-icon.png' width='50px' height='50px'>";
         saveBtn.addEventListener("click", function(){storeEventLocal(this.id)});
     }
@@ -61,11 +70,13 @@ function updateDescColor() {
         var eventDesc = document.getElementById(textareaid);
         var eachHour = i + 8;
         if( eachHour < currentHour ){
-            eventDesc.removeAttribute('class', 'future');
-            eventDesc.setAttribute('class', 'past');
-        }else if( eachHour === currentHour ){
-            eventDesc.removeAttribute('class', 'future');
-            eventDesc.setAttribute('class', 'present');
+        // Remove and add classes from list whilst keeping class list otherwise intact
+            eventDesc.classList.remove('future');
+            eventDesc.classList.add('past');
+        // Loose equality here because moment returns a string.    
+        }else if( eachHour == currentHour ){
+            eventDesc.classList.remove('future');
+            eventDesc.classList.add('present');
         }
     } 
 }
